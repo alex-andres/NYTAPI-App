91cc16df6e03e052ccc456992f8cd3c319c699ac
BakerShoeMaker <info@quackenworth.com>
2017-09-22 10:28:46 -0700

    Added initial structure to index, synced with bootstrap/jquery libraries, and app.js. Add to readMe file.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# NYTAPI-App
+# NYTAPI-App  
+
+This is app allows the user to search the New York Times database using certain search parameters.
+The app uses the New York Times search API.
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
-	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+console.log("Hello world!");
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 		url += '?' + $.param({
   		'api-key': "b9f91d369ff59547cd47b931d8cbc56b:0:74623931",
   		'q': "Snow Leopards",
