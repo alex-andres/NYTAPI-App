9ed80bebc73c21697e26605f1bac9824e8d6e54d
Ali Darvishi <erfaanism@gmail.com>
2017-09-23 21:55:12 -0700

    completed the design

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,106 +1,108 @@
-console.log("Hello world!");
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-		url += '?' + $.param({
-  		'api-key': "b9f91d369ff59547cd47b931d8cbc56b:0:74623931",
-  		'q': "Snow",
-  		
-});
-		var begin_date;
-		var end_date = 2000;
-
-		if (begin_date > 1850 && begin_date < 2017) {
-			begin_date.toString;
-			begin_date += "0101";
-
-		{
-			$.param[2] = {"begin_date" : begin_date};
-		}
-
-		url += $.param[2];
-
-		}
-
-			if (end_date > begin_date && end_date < 2017) {
-				end_date.toString;
-				end_date += "1231";
-
-		{
-			$.param[3] = {"end_date" : end_date};
-		}
-
-		url += $.param[3];
-
-		}
-
-		else if (end_date > 1851 && end_date < 2017) {
-				end_date.toString;
-				end_date += "1231";
-
-		{
-			$.param[2] = {"end_date" : end_date};
-		}
-
-		url += $.param[2];
-
-		}
-
+$(document).ready(function() {
+	console.log("Hello world!");
+	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+			url += '?' + $.param({
+			'api-key': "b9f91d369ff59547cd47b931d8cbc56b:0:74623931",
+			'q': "Snow",
 			
-		var queryResults = 15;
+	});
+	var begin_date;
+	var end_date = 2000;
 
-var articleObject = "";
+	if (begin_date > 1850 && begin_date < 2017) {
+		begin_date.toString;
+		begin_date += "0101";
 
-if (queryResults > 10) {
+	{
+		$.param[2] = {"begin_date" : begin_date};
+	}
 
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
+	url += $.param[2];
 
-for (var i = 0; i < 10; i++) {
-  	console.log(result.response.docs[i].headline.main);
-  	console.log(result.response.docs[i].web_url)
-  	console.log(result.response.docs[i].snippet)
-  }
-}).fail(function(err) {
-  throw err;
-});
+	}
+	
+	if (end_date > begin_date && end_date < 2017) {
+		end_date.toString;
+		end_date += "1231";
 
-$.param[3] = {"page" : 11};
+	{
+		$.param[3] = {"end_date" : end_date};
+	}
+
+	url += $.param[3];
+
+	}
+
+	else if (end_date > 1851 && end_date < 2017) {
+		end_date.toString;
+		end_date += "1231";
+
+	{
+		$.param[2] = {"end_date" : end_date};
+	}
+
+	url += $.param[2];
+
+	}
+
 		
+	var queryResults = 15;
 
-		url += $.param[3];
+	var articleObject = "";
 
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
+	if (queryResults > 10) {
 
-for (var i = 0; i < queryResults-10; i++) {
-  	console.log(result.response.docs[i].headline.main);
-  	console.log(result.response.docs[i].web_url)
-  	console.log(result.response.docs[i].snippet)
-  }
-}).fail(function(err) {
-  throw err;
-});
-
-}
-
-else {
 	$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
+		url: url,
+		method: 'GET',
+	}).done(function(result) {
+		console.log(result);
 
-for (var i = 0; i < 10; i++) {
-  	console.log(result.response.docs[i].headline.main);
-  	console.log(result.response.docs[i].web_url)
-  	console.log(result.response.docs[i].snippet)
-  }
-}).fail(function(err) {
-  throw err;
-});
-}
+	for (var i = 0; i < 10; i++) {
+		console.log(result.response.docs[i].headline.main);
+		console.log(result.response.docs[i].web_url)
+		console.log(result.response.docs[i].snippet)
+		}
+	}).fail(function(err) {
+		throw err;
+	});
+
+	$.param[3] = {"page" : 11};
+			
+	url += $.param[3];
+
+	$.ajax({
+		url: url,
+		method: 'GET',
+	}).done(function(result) {
+		console.log(result);
+
+	for (var i = 0; i < queryResults-10; i++) {
+		console.log(result.response.docs[i].headline.main);
+		console.log(result.response.docs[i].web_url)
+		console.log(result.response.docs[i].snippet)
+		}
+	}).fail(function(err) {
+		throw err;
+	});
+
+	}
+
+	else {
+		$.ajax({
+		url: url,
+		method: 'GET',
+	}).done(function(result) {
+		console.log(result);
+
+	for (var i = 0; i < 10; i++) {
+		console.log(result.response.docs[i].headline.main);
+		console.log(result.response.docs[i].web_url)
+		console.log(result.response.docs[i].snippet)
+	}
+
+	}).fail(function(err) {
+		throw err;
+	});
+	}
+})
